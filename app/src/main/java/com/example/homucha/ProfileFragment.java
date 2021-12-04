5d22e0e460f4f8083febdc8d0f71d62c6fef30c1
devidwiyani <devidwiyani52@gmail.com>
2021-12-04 11:41:41 +0800

    edit halaman profil

--- app/src/main/java/com/example/homucha/ProfileFragment.java
+++ app/src/main/java/com/example/homucha/ProfileFragment.java
@@ -1,19 +1,50 @@
 package com.example.homucha;
 
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
 
+    RelativeLayout expandableView;
+    ImageButton arrowBtn;
+    CardView cardView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        expandableView = v.findViewById(R.id.expandableView);
+        arrowBtn = v.findViewById(R.id.arrowBtn);
+        cardView = v.findViewById(R.id.cardView);
+
+
+        arrowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (expandableView.getVisibility()==View.GONE){
+                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                    expandableView.setVisibility(View.VISIBLE);
+                    arrowBtn.setBackgroundResource(R.drawable.ic_arrow_drop_up);
+                } else {
+                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                    expandableView.setVisibility(View.GONE);
+                    arrowBtn.setBackgroundResource(R.drawable.ic_arrow_drop);
+                }
+            }
+        });
+
+        return v;
     }
 }
