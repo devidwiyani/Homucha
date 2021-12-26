24924896e9407fcb73c5cdb563549c3c8f655c10
dianmediana <82147780+dianmediana@users.noreply.github.com>
2021-12-26 17:53:46 +0800

    layout list produk

--- app/src/main/java/com/example/homucha/DashboardActivity.java
+++ app/src/main/java/com/example/homucha/DashboardActivity.java
@@ -63,7 +63,7 @@ public class DashboardActivity extends AppCompatActivity {
             {
                 //SOFA
                 dbWrite.execSQL("INSERT INTO tb_produk VALUES(1,1," +
-                        "'CASABEL Estrella',19400000," +
+                        "'CASABEL Estrella', 19400000," +
                         "'ÄPPLARYD sofa will be your home’s comfy oasis. An eye-catcher that reflects your personality and style. Great to sit, lie down and hang out on. And with lots of space for the whole family, year after year.',"+R.drawable.sofa_hitam+")");
                 dbWrite.execSQL("INSERT INTO tb_produk VALUES(2,1," +
                         "'Ebonia',2825000," +
