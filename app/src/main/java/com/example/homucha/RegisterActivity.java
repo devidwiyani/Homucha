8ca902e9d8f04b905cb237a47f87fcba88ad0577
Pandu Pratama <pandupratama963@gmail.com>
2021-12-26 23:32:55 +0800

    baru inget nambahin validasi pas register biar gk ada username yang sama

--- app/src/main/java/com/example/homucha/RegisterActivity.java
+++ app/src/main/java/com/example/homucha/RegisterActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,7 +28,6 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
         regUsername = findViewById(R.id.rgUsername);
         regPassword = findViewById(R.id.rgPassword);
         regName = findViewById(R.id.rgName);
@@ -36,7 +37,7 @@ public class RegisterActivity extends AppCompatActivity {
         btnRegister = findViewById(R.id.btn_register);
 
         dbHelper = new DbHelper(this);
-
+        SQLiteDatabase dbRead = dbHelper.getReadableDatabase();
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,27 +51,35 @@ public class RegisterActivity extends AppCompatActivity {
                 boolean check = validasiForm(ambilUsername, ambilPassword, ambilName,
                         ambilAddress, ambilPhone, ambilEmail);
                 if (check == true) {
+                    Cursor checkAccountAvailable = dbRead.rawQuery("SELECT*FROM tb_user WHERE username = '"+ambilUsername+"'",null);
+                    if(checkAccountAvailable.getCount() == 0)
+                    {
+                        ContentValues values = new ContentValues();
+                        values.put(dbHelper.row_username, ambilUsername);
+                        values.put(dbHelper.row_password, ambilPassword);
+                        values.put(dbHelper.row_name, ambilName);
+                        values.put(dbHelper.row_address, ambilAddress);
+                        values.put(dbHelper.row_phone, ambilPhone);
+                        values.put(dbHelper.row_email, ambilEmail);
+                        dbHelper.insertDataUser(values);
 
-                    ContentValues values = new ContentValues();
-                    values.put(dbHelper.row_username, ambilUsername);
-                    values.put(dbHelper.row_password, ambilPassword);
-                    values.put(dbHelper.row_name, ambilName);
-                    values.put(dbHelper.row_address, ambilAddress);
-                    values.put(dbHelper.row_phone, ambilPhone);
-                    values.put(dbHelper.row_email, ambilEmail);
-                    dbHelper.insertDataUser(values);
+                        Intent intent = new Intent(RegisterActivity.this,
+                                DashboardActivity.class);
+                        Toast.makeText(getApplicationContext(), "Register Berhasil!", Toast.LENGTH_SHORT).show();
+                        intent.putExtra("username", ambilUsername);
+                        intent.putExtra("password", ambilPassword);
+                        intent.putExtra("name", ambilName);
+                        intent.putExtra("address", ambilAddress);
+                        intent.putExtra("phone", ambilPhone);
+                        intent.putExtra("email", ambilEmail);
+                        spm.saveSPInt(getBaseContext(), spm.SP_ID, dbHelper.checkUserId(ambilUsername, ambilPassword));
+                        startActivity(intent);
+                    }
+                    else
+                    {
+                        Toast.makeText(view.getContext(), "Username Akun Ini Sudah Terdaftar. Silahkan Gunakan Username Lain", Toast.LENGTH_SHORT).show();
+                    }
 
-                    Intent intent = new Intent(RegisterActivity.this,
-                            LoginActivity.class);
-                    Toast.makeText(getApplicationContext(), "Register Berhasil!", Toast.LENGTH_SHORT).show();
-                    intent.putExtra("username", ambilUsername);
-                    intent.putExtra("password", ambilPassword);
-                    intent.putExtra("name", ambilName);
-                    intent.putExtra("address", ambilAddress);
-                    intent.putExtra("phone", ambilPhone);
-                    intent.putExtra("email", ambilEmail);
-                    spm.saveSPInt(getBaseContext(), spm.SP_ID, dbHelper.checkUserId(ambilUsername, ambilPassword));
-                    startActivity(intent);
                 }
 
             }
